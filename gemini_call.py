085970ae68d319205494df0085f48431106c1f79
Nura <nkawa@berkeley.edu>
2025-03-31 16:46:18 +0200

    update gemini call prompt

--- gemini_call.py
+++ gemini_call.py
@@ -3,6 +3,21 @@ from google.genai import types
 import base64
 from openai import OpenAI
 import requests
+import json
+
+
+def convert_place_info(place_json):
+  """
+  Input `place_json`: a JSON file with place information + reviews
+  Output: text
+  """
+  
+  with open(place_json, 'r', encoding='utf-8') as file:
+    data = json.load(file)  # Load JSON data into a Python object
+  
+  text_output = json.dumps(data, indent=4)
+  
+  return text_output
 
 
 def compare_requirements_with_place_info(requirements=None, place_info=None):
@@ -12,14 +27,36 @@ def compare_requirements_with_place_info(requirements=None, place_info=None):
     location="us-central1",
   )
 
-  text1 = types.Part.from_text(text="""You are an expert Translator. You are tasked to translate documents from en to fr.Please provide an accurate translation of this document and return translation text only:""")
+  text1 = types.Part.from_text(text=
+                               """You are an expert travel advisor, and you love to make personalized recommendations for users.
+                               You have a favorite cafe for which you have a set of reviews {place_info}.
+                               A user gives you his requirements for the perfect cafe {requirements}.
+
+                               Your task is to give the user a rating between 0 and 5 starts of how well does your favorite cafe match his requirements.
+
+                               You must return: 
+                               - The name of your favorite cafe. 
+                               - The personalized rating that you would give for that cafe. 
+                               - An example from the {place_info} that explains why you would give that rating.
+                               """)
+
+  
+  text2 = types.Part.from_text(text=requirements)
+
+  place_info = convert_place_info(place_info)
+
+  text3 = types.Part.from_text(text=place_info)
+
+
 
   model = "gemini-2.0-flash-lite-001"
   contents = [
     types.Content(
       role="user",
       parts=[
-        text1
+        text1,
+        text2,
+        text3
       ]
     )
   ]
@@ -34,7 +71,13 @@ def compare_requirements_with_place_info(requirements=None, place_info=None):
     model = model,
     contents = contents,
     config = generate_content_config,
-    )
+  )
+
+  print(response.text)
+
   return response.text
+  #return response.text
 
 
+compare_requirements_with_place_info(requirements="dog-friendly, laptop friendly, cheap, serves a cappuccino", 
+                                       place_info='result.json')
